82a8e2e9c5b2884e0c4b805b802cc60aa85ce0a0
ArunDoddi <arundoddi456@gmail.com>
2025-05-19 15:46:23 -0400

    My Daily Grind of Java 8 and 17 of LeetCode Practices and other problems

---
+++ test/validPalindromeTests.java
@@ -0,0 +1,12 @@
+import com.LeetCode.two_pointers.ValidPalindrome_14ms;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class validPalindromeTests {
+    ValidPalindrome_14ms vp14 = new ValidPalindrome_14ms();
+
+    @Test
+    public void testCase1(){
+        assertTrue(true, "Marge, let's \\\"[went].\\\" I await {news} telegram.");
+    }
+}
